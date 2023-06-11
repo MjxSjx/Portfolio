e2db1f7a834ccfb2b57d2a8643a517bce627007e
Matt Johnson <111515875+MjxSjx@users.noreply.github.com>
2023-06-11 06:06:39 -0500

    Update Mean value of 'ride_length' across all sheets.sql

--- Case Study 1 - bike-share analysis/SQL Results/Individual Statistical Details/Mean value of 'ride_length' across all sheets.sql
+++ Case Study 1 - bike-share analysis/SQL Results/Individual Statistical Details/Mean value of 'ride_length' across all sheets.sql
@@ -1,5 +1,5 @@
 -- The mean is affected by outlier field entries that contain 23+ hours. No dynamic version, as its end result was consistently wrong.
-
+ 
 SELECT AVG(CONVERT(BIGINT, DATEDIFF(SECOND, CAST('00:00:00' AS TIME), ride_length))) AS MeanRL FROM (
     SELECT ride_length FROM [202205-divvy-tripdata]
 		UNION
