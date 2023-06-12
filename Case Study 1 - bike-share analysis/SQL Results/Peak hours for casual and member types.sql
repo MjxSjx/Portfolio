d785d9eef22e835e07d34a73fa45a7d16f56333e
Matt Johnson <111515875+MjxSjx@users.noreply.github.com>
2023-06-12 10:40:46 -0500

    Update Peak hours for casual and member types.sql

--- Case Study 1 - bike-share analysis/SQL Results/Peak hours for casual and member types.sql
+++ Case Study 1 - bike-share analysis/SQL Results/Peak hours for casual and member types.sql
@@ -7,80 +7,80 @@ SELECT
   member_casual,
   COUNT(*) AS totalRides
 FROM (
-    SELECT started_at, member_casual FROM [202205-divvy-tripdata]
+    SELECT started_at, member_casual FROM [202205-tripdata]
     UNION ALL
-    SELECT started_at, member_casual FROM [202206-divvy-tripdata]
+    SELECT started_at, member_casual FROM [202206-tripdata]
     UNION ALL
-    SELECT started_at, member_casual FROM [202207-divvy-tripdata]
+    SELECT started_at, member_casual FROM [202207-tripdata]
     UNION ALL
-    SELECT started_at, member_casual FROM [202208-divvy-tripdata]
+    SELECT started_at, member_casual FROM [202208-tripdata]
     UNION ALL
-    SELECT started_at, member_casual FROM [202209-divvy-tripdata]
+    SELECT started_at, member_casual FROM [202209-tripdata]
     UNION ALL
-    SELECT started_at, member_casual FROM [202210-divvy-tripdata]
+    SELECT started_at, member_casual FROM [202210-tripdata]
     UNION ALL
-    SELECT started_at, member_casual FROM [202211-divvy-tripdata]
+    SELECT started_at, member_casual FROM [202211-tripdata]
     UNION ALL
-    SELECT started_at, member_casual FROM [202212-divvy-tripdata]
+    SELECT started_at, member_casual FROM [202212-tripdata]
     UNION ALL
-    SELECT started_at, member_casual FROM [202301-divvy-tripdata]
+    SELECT started_at, member_casual FROM [202301-tripdata]
     UNION ALL
-    SELECT started_at, member_casual FROM [202302-divvy-tripdata]
+    SELECT started_at, member_casual FROM [202302-tripdata]
     UNION ALL
-    SELECT started_at, member_casual FROM [202303-divvy-tripdata]
+    SELECT started_at, member_casual FROM [202303-tripdata]
     UNION ALL
-    SELECT started_at, member_casual FROM [202304-divvy-tripdata]
+    SELECT started_at, member_casual FROM [202304-tripdata]
 ) AS AnnualTable
 WHERE member_casual IN ('member', 'casual')
 GROUP BY DATEPART(HOUR, started_at), member_casual
 ORDER BY totalRides DESC;
 
 hour	member_casual	totalRides
-5pm	    member	  369308
-4pm	    member	  309434
-6pm	    member	  297032
-3pm	    member	  233464
-5pm	    casual	  225217
-8am	    member	  217011
-7pm	    member	  215124
-4pm	    casual	  202483
-6pm	    casual	  200178
-12pm	    member	  193296
-2pm	    member	  192615
-1pm	    member	  192485
-3pm	    casual	  180325
-7am	    member	  180001
-11am	    member	  168286
-2pm	    casual	  161060
-5pm	    casual	  153534
-9am	    member	  152399
-1pm	    casual	  151462
-8pm	    member	  150633
-12pm	    casual	  145066
-10am	    member	  142095
-11am	    casual	  122314
-9pm	    member	  118248
-8pm	    casual	  113439
-9pm	    casual	  97208
-6am	    member	  96861
-10am	    casual	  94619
-10pm	    member	  89837
-10pm	    casual	  86600
-9am	    casual	  74458
-8am	    casual	  72823
-11pm	    casual	  64362
-11pm	    member	  58585
-7am	    casual	  54059
-12am	    casual	  47113
-12am	    member	  37567
-5am	    member	  32719
-6am	    casual	  31240
-1am	    casual	  30410
-1am	    member	  22938
-2am	    casual	  18802
-2am	    member	  13482
-5am	    casual	  12848
-3am	    casual	  11007
-4am	    member	  9087
-3am	    member	  8247
-4am	    casual	  7680
+5pm	    member	  296994
+4pm	    member	  247102
+6pm	    member	  234026
+3pm	    member	  182442
+8am	    member	  177202
+5pm	    casual	  173877
+7pm	    member	  166912
+4pm	    casual	  156011
+6pm	    casual	  152303
+7am	    member	  149847
+12pm	    member	  149679
+2pm	    member	  148311
+1pm	    member	  148054
+3pm	    casual	  137444
+11am	    member	  130131
+2pm	    casual	  123776
+9am	    member	  119318
+1pm	    casual	  117104
+7pm	    member	  115921
+8pm     member     115792
+12pm	    casual	  112524
+10am	    member	  109708
+11am	    casual	  95578
+9pm	    member	  89465
+8pm	    casual	 84662
+6am	    member	  80509
+10am	    casual	  80509
+9pm	    casual	  72390
+10pm	    member	  66397
+10pm	    casual	 64289
+9am	    casual	  56799
+8am	    casual	  55166
+11pm	    casual	  46990
+11pm	    member	  41998
+7am	    casual	  40109
+12am	    casual	  33942
+12am	    member	  26275
+5am	    member	  26170
+6am	    casual	  23211
+1am	    casual	  21597
+1am	    member	  21597
+2am	    casual	  16029
+2am	    member	  8947
+5am	    casual	 8768
+3am	    casual	  7139
+4am	    member	  6178
+3am	    member	  5366
+4am	    casual	  4733
