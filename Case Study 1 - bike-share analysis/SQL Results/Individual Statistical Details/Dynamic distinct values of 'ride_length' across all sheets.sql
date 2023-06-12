395178fce61ae79e22c0cb88672c0bc38d91c7b2
Matt Johnson <111515875+MjxSjx@users.noreply.github.com>
2023-06-12 09:06:47 -0500

    Update Dynamic distinct values of 'ride_length' across all sheets.sql

--- Case Study 1 - bike-share analysis/SQL Results/Individual Statistical Details/Dynamic distinct values of 'ride_length' across all sheets.sql
+++ Case Study 1 - bike-share analysis/SQL Results/Individual Statistical Details/Dynamic distinct values of 'ride_length' across all sheets.sql
@@ -26,9 +26,9 @@ SELECT COUNT(DISTINCT ride_length) AS DistinctRL FROM (
   SELECT ride_length FROM [202304-tripdata]
 ) AS AnnualData;
 DistinctRL
+19503
 
 ---------------------------------------------------------------------------------------------------
-
 DECLARE @sql NVARCHAR(MAX);
 SET @sql = N'SELECT COUNT(DISTINCT ride_length) AS DistinctRL FROM (';
 
@@ -37,10 +37,10 @@ SELECT @sql = @sql + N'
     UNION'
 FROM sys.tables
 WHERE name IN (
-    '202205-divvy-tripdata', '202206-divvy-tripdata', '202207-divvy-tripdata',
-    '202208-divvy-tripdata', '202209-divvy-tripdata', '202210-divvy-tripdata',
-    '202211-divvy-tripdata', '202212-divvy-tripdata', '202301-divvy-tripdata',
-    '202302-divvy-tripdata', '202303-divvy-tripdata', '202304-divvy-tripdata'
+    '202205-tripdata', '202206-tripdata', '202207-tripdata',
+    '202208-tripdata', '202209-tripdata', '202210-tripdata',
+    '202211-tripdata', '202212-tripdata', '202301-tripdata',
+    '202302-tripdata', '202303-tripdata', '202304-tripdata'
 );
 
 SET @sql = LEFT(@sql, LEN(@sql) - LEN('UNION'));
@@ -49,4 +49,4 @@ SET @sql = @sql + N') AS AnnualData;'
 
 EXEC sp_executesql @sql;
 DistinctRL
-19601
+19503
