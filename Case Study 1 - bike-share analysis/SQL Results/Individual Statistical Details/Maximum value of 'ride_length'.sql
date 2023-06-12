84bda12b70ba0e6198576011269fc6b0aef69f06
Matt Johnson <111515875+MjxSjx@users.noreply.github.com>
2023-06-12 09:42:55 -0500

    Update Maximum value of 'ride_length'.sql

--- Case Study 1 - bike-share analysis/SQL Results/Individual Statistical Details/Maximum value of 'ride_length'.sql
+++ Case Study 1 - bike-share analysis/SQL Results/Individual Statistical Details/Maximum value of 'ride_length'.sql
@@ -1,38 +1,49 @@
---The suggestion would be to remove a layer of the outliers in the data. 
+--The suggestion would be to remove a layer of the outliers in the data.   
 
 SELECT MAX(ride_length) AS MaxRL FROM [202205-tripdata];
 MaxRL
 23:57:42
+
 SELECT MAX(ride_length) AS MaxRL FROM [202206-tripdata];
+MaxRL
 23:58:49
-MaxRL
+
 SELECT MAX(ride_length) AS MaxRL FROM [202207-tripdata];
+MaxRL
 23:59:22
-MaxRL
+
 SELECT MAX(ride_length) AS MaxRL FROM [202208-tripdata];
-23:56:57
 MaxRL
+23:56:57
+
 SELECT MAX(ride_length) AS MaxRL FROM [202209-tripdata];
 MaxRL
 23:35:27
+
 SELECT MAX(ride_length) AS MaxRL FROM [202210-tripdata];
 MaxRL
 23:56:20
+
 SELECT MAX(ride_length) AS MaxRL FROM [202211-tripdata];
 MaxRL
 23:57:35
+
 SELECT MAX(ride_length) AS MaxRL FROM [202212-tripdata];
 MaxRL
 23:26:21
+
 SELECT MAX(ride_length) AS MaxRL FROM [202301-tripdata];
 MaxRL
 23:42:34
+
 SELECT MAX(ride_length) AS MaxRL FROM [202302-tripdata];
 MaxRL
 23:43:47
+
 SELECT MAX(ride_length) AS MaxRL FROM [202303-tripdata];
 MaxRL
 23:47:34
+
 SELECT MAX(ride_length) AS MaxRL FROM [202304-tripdata];
 MaxRL
 23:53:18
