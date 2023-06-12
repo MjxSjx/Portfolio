5e835f43a08bcef12f00055694889076c8407c5c
Matt Johnson <111515875+MjxSjx@users.noreply.github.com>
2023-06-12 03:42:27 -0500

    Update Distinct values of 'day_of_week'.sql

--- Case Study 1 - bike-share analysis/SQL Results/Individual Statistical Details/Distinct values of 'day_of_week'.sql
+++ Case Study 1 - bike-share analysis/SQL Results/Individual Statistical Details/Distinct values of 'day_of_week'.sql
@@ -1,38 +1,38 @@
 -- I'm here to report the data, not judge. That being said, the bikes were rented out all seven days of the week every month.
  
-SELECT COUNT(DISTINCT day_of_week) AS DistinctDoW FROM [202205-divvy-tripdata];
+SELECT COUNT(DISTINCT day_of_week) AS DistinctDoW FROM [202205-tripdata];
 DistinctDoW
 7
-SELECT COUNT(DISTINCT day_of_week) AS DistinctDoW FROM [202206-divvy-tripdata];
+SELECT COUNT(DISTINCT day_of_week) AS DistinctDoW FROM [202206-tripdata];
 DistinctDoW
 7
-SELECT COUNT(DISTINCT day_of_week) AS DistinctDoW FROM [202207-divvy-tripdata];
+SELECT COUNT(DISTINCT day_of_week) AS DistinctDoW FROM [202207-tripdata];
 DistinctDoW
 7
-SELECT COUNT(DISTINCT day_of_week) AS DistinctDoW FROM [202208-divvy-tripdata];
+SELECT COUNT(DISTINCT day_of_week) AS DistinctDoW FROM [202208-tripdata];
 DistinctDoW
 7
-SELECT COUNT(DISTINCT day_of_week) AS DistinctDoW FROM [202209-divvy-tripdata];
+SELECT COUNT(DISTINCT day_of_week) AS DistinctDoW FROM [202209-tripdata];
 DistinctDoW
 7
-SELECT COUNT(DISTINCT day_of_week) AS DistinctDoW FROM [202210-divvy-tripdata];
+SELECT COUNT(DISTINCT day_of_week) AS DistinctDoW FROM [202210-tripdata];
 DistinctDoW
 7
-SELECT COUNT(DISTINCT day_of_week) AS DistinctDoW FROM [202211-divvy-tripdata];
+SELECT COUNT(DISTINCT day_of_week) AS DistinctDoW FROM [202211-tripdata];
 DistinctDoW
 7
-SELECT COUNT(DISTINCT day_of_week) AS DistinctDoW FROM [202212-divvy-tripdata];
+SELECT COUNT(DISTINCT day_of_week) AS DistinctDoW FROM [202212-tripdata];
 DistinctDoW
 7
-SELECT COUNT(DISTINCT day_of_week) AS DistinctDoW FROM [202301-divvy-tripdata];
+SELECT COUNT(DISTINCT day_of_week) AS DistinctDoW FROM [202301-tripdata];
 DistinctDoW
 7
-SELECT COUNT(DISTINCT day_of_week) AS DistinctDoW FROM [202302-divvy-tripdata];
+SELECT COUNT(DISTINCT day_of_week) AS DistinctDoW FROM [202302-tripdata];
 DistinctDoW
 7
-SELECT COUNT(DISTINCT day_of_week) AS DistinctDoW FROM [202303-divvy-tripdata];
+SELECT COUNT(DISTINCT day_of_week) AS DistinctDoW FROM [202303-tripdata];
 DistinctDoW
 7
-SELECT COUNT(DISTINCT day_of_week) AS DistinctDoW FROM [202304-divvy-tripdata];
+SELECT COUNT(DISTINCT day_of_week) AS DistinctDoW FROM [202304-tripdata];
 DistinctDoW
 7
