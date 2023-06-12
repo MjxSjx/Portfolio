5a7cef3917c029d8ad2cf64d2d52f226365d7d4f
Matt Johnson <111515875+MjxSjx@users.noreply.github.com>
2023-06-12 09:29:04 -0500

    Update Minimum value of 'ride_length'.sql

--- Case Study 1 - bike-share analysis/SQL Results/Individual Statistical Details/Minimum value of 'ride_length'.sql
+++ Case Study 1 - bike-share analysis/SQL Results/Individual Statistical Details/Minimum value of 'ride_length'.sql
@@ -3,85 +3,96 @@
 -- However, when querying further, I found MIN(ride_length) at :02, :03, :04, and on.
 SELECT 
 	MIN(ride_length) AS MinRL 
-	FROM (SELECT ride_length FROM [202205-divvy-tripdata] 
-	WHERE ride_length > '00:00:00') 
-	AS subquery;
-MinRL
-00:01:00
-SELECT 
-	MIN(ride_length) AS MinRL 
-	FROM (SELECT ride_length FROM [202206-divvy-tripdata] 
-	WHERE ride_length > '00:00:00') 
-	AS subquery;
+	FROM (SELECT ride_length FROM [202205-tripdata]
+	WHERE ride_length > '00:00:00'
+) subquery;
 MinRL
 00:00:01
+
 SELECT 
 	MIN(ride_length) AS MinRL 
-	FROM (SELECT ride_length FROM [202207-divvy-tripdata] 
-	WHERE ride_length > '00:00:00') 
-	AS subquery;
+	FROM (SELECT ride_length FROM [202206-tripdata]
+	WHERE ride_length > '00:00:00'
+) subquery;
 MinRL
 00:00:01
+
 SELECT 
 	MIN(ride_length) AS MinRL 
-	FROM (SELECT ride_length FROM [202208-divvy-tripdata] 
-	WHERE ride_length > '00:00:00') 
-	AS subquery;
+	FROM (SELECT ride_length FROM [202207-tripdata]
+	WHERE ride_length > '00:00:00' 
+) subquery;
 MinRL
 00:00:01
+
 SELECT 
 	MIN(ride_length) AS MinRL 
-	FROM (SELECT ride_length FROM [202209-divvy-tripdata] 
-	WHERE ride_length > '00:00:00') 
-	AS subquery;
+	FROM (SELECT ride_length FROM [202208-tripdata]
+	WHERE ride_length > '00:00:00' 
+) subquery;
 MinRL
 00:00:01
+
 SELECT 
 	MIN(ride_length) AS MinRL 
-	FROM (SELECT ride_length FROM [202210-divvy-tripdata] 
-	WHERE ride_length > '00:00:00') 
-	AS subquery;
+	FROM (SELECT ride_length FROM [202209-tripdata]
+	WHERE ride_length > '00:00:00'
+) subquery;
 MinRL
 00:00:01
+
 SELECT 
 	MIN(ride_length) AS MinRL 
-	FROM (SELECT ride_length FROM [202211-divvy-tripdata] 
-	WHERE ride_length > '00:00:00') 
-	AS subquery;
+	FROM (SELECT ride_length FROM [202210-tripdata] 
+	WHERE ride_length > '00:00:00' 
+) subquery;
 MinRL
 00:00:01
+
 SELECT 
 	MIN(ride_length) AS MinRL 
-	FROM (SELECT ride_length FROM [202212-divvy-tripdata] 
-	WHERE ride_length > '00:00:00') 
-	AS subquery;
+	FROM (SELECT ride_length FROM [202211-tripdata]
+	WHERE ride_length > '00:00:00' 
+) subquery;
 MinRL
 00:00:01
+
 SELECT 
 	MIN(ride_length) AS MinRL 
-	FROM (SELECT ride_length FROM [202301-divvy-tripdata]	 
-	WHERE ride_length > '00:00:00') 
-	AS subquery;
+	FROM (SELECT ride_length FROM [202212-tripdata]
+	WHERE ride_length > '00:00:00'
+) subquery;
 MinRL
 00:00:01
+
 SELECT 
 	MIN(ride_length) AS MinRL 
-	FROM (SELECT ride_length FROM [202302-divvy-tripdata] 
-	WHERE ride_length > '00:00:00') 
-	AS subquery;
+	FROM (SELECT ride_length FROM [202301-tripdata]	 
+	WHERE ride_length > '00:00:00' 
+) subquery;
 MinRL
 00:00:01
+
 SELECT 
 	MIN(ride_length) AS MinRL 
-	FROM (SELECT ride_length FROM [202303-divvy-tripdata] 
-	WHERE ride_length > '00:00:00') 
-	AS subquery;
+	FROM (SELECT ride_length FROM [202302-tripdata] 
+	WHERE ride_length > '00:00:00'
+) subquery;
 MinRL
 00:00:01
+
 SELECT 
 	MIN(ride_length) AS MinRL 
-	FROM (SELECT ride_length FROM [202304-divvy-tripdata] 
-	WHERE ride_length > '00:00:00') 
-	AS subquery;
+	FROM (SELECT ride_length FROM [202303-tripdata]
+	WHERE ride_length > '00:00:00'
+) subquery;
+MinRL
+00:00:01
+
+SELECT 
+	MIN(ride_length) AS MinRL 
+	FROM (SELECT ride_length FROM [202304-tripdata]
+	WHERE ride_length > '00:00:00' 
+) subquery;
 MinRL
 00:00:01
