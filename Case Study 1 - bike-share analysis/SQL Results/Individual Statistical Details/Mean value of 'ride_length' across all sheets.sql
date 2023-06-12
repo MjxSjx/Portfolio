7676c2bd0cbe2f60d83eb6f21ebdfab32ed98bd4
Matt Johnson <111515875+MjxSjx@users.noreply.github.com>
2023-06-12 06:22:27 -0500

    Update Mean value of 'ride_length' across all sheets.sql

--- Case Study 1 - bike-share analysis/SQL Results/Individual Statistical Details/Mean value of 'ride_length' across all sheets.sql
+++ Case Study 1 - bike-share analysis/SQL Results/Individual Statistical Details/Mean value of 'ride_length' across all sheets.sql
@@ -1,29 +1,30 @@
--- The mean is affected by outlier field entries that contain 23+ hours. No dynamic version, as its end result was consistently wrong.
+-- The mean is affected by outlier field entries that contain 24+ hours. No dynamic version, as its end result was consistently wrong.
  
-SELECT AVG(CONVERT(BIGINT, DATEDIFF(SECOND, CAST('00:00:00' AS TIME), ride_length))) AS MeanRL FROM (
-    SELECT ride_length FROM [202205-divvy-tripdata]
-		UNION
-    SELECT ride_length FROM [202206-divvy-tripdata]
-		UNION
-    SELECT ride_length FROM [202207-divvy-tripdata]
-		UNION
-    SELECT ride_length FROM [202208-divvy-tripdata]
-		UNION
-    SELECT ride_length FROM [202209-divvy-tripdata]
-		UNION
-    SELECT ride_length FROM [202210-divvy-tripdata]
-		UNION
-    SELECT ride_length FROM [202211-divvy-tripdata]
-		UNION
-    SELECT ride_length FROM [202212-divvy-tripdata]
-		UNION
-    SELECT ride_length FROM [202301-divvy-tripdata]	
-		UNION
-    SELECT ride_length FROM [202302-divvy-tripdata]	
-		UNION
-    SELECT ride_length FROM [202303-divvy-tripdata]
-		UNION
-    SELECT ride_length FROM [202304-divvy-tripdata]
+SELECT AVG(CONVERT(BIGINT, DATEDIFF(SECOND, CAST('00:00:00' AS TIME), TRY_CONVERT(TIME, ride_length)))) AS MeanRL
+FROM (
+    SELECT ride_length FROM [202205-tripdata]
+    UNION
+    SELECT ride_length FROM [202206-tripdata]
+    UNION
+    SELECT ride_length FROM [202207-tripdata]
+    UNION
+    SELECT ride_length FROM [202208-tripdata]
+    UNION
+    SELECT ride_length FROM [202209-tripdata]
+    UNION
+    SELECT ride_length FROM [202210-tripdata]
+    UNION
+    SELECT ride_length FROM [202211-tripdata]
+    UNION
+    SELECT ride_length FROM [202212-tripdata]
+    UNION
+    SELECT ride_length FROM [202301-tripdata]    
+    UNION
+    SELECT ride_length FROM [202302-tripdata]    
+    UNION
+    SELECT ride_length FROM [202303-tripdata]
+    UNION
+    SELECT ride_length FROM [202304-tripdata]
 ) AS AnnualData;
 MeanRL
-04:21:04
+15550 aka 4 hours and 19 minutes
