f94e9d6fcd27544617b0468c05e743bc03474426
Matt Johnson <111515875+MjxSjx@users.noreply.github.com>
2023-06-12 10:08:31 -0500

    Update Group by rideable_type total per type.sql

--- Case Study 1 - bike-share analysis/SQL Results/Group by rideable_type total per type.sql
+++ Case Study 1 - bike-share analysis/SQL Results/Group by rideable_type total per type.sql
@@ -1,33 +1,33 @@
--- Electric bikes are the most popular product of the three.
+-- Classic bikes are the most popular product of the three.
   
 SELECT rideable_type, COUNT(*) AS totalRides
 FROM (
-    SELECT rideable_type FROM [202205-divvy-tripdata]
+    SELECT rideable_type FROM [202205-tripdata]
     UNION ALL
-    SELECT rideable_type FROM [202206-divvy-tripdata]
+    SELECT rideable_type FROM [202206-tripdata]
     UNION ALL
-    SELECT rideable_type FROM [202207-divvy-tripdata]
+    SELECT rideable_type FROM [202207-tripdata]
     UNION ALL
-    SELECT rideable_type FROM [202208-divvy-tripdata]
+    SELECT rideable_type FROM [202208-tripdata]
     UNION ALL
-    SELECT rideable_type FROM [202209-divvy-tripdata]
+    SELECT rideable_type FROM [202209-tripdata]
     UNION ALL
-    SELECT rideable_type FROM [202210-divvy-tripdata]
+    SELECT rideable_type FROM [202210-tripdata]
     UNION ALL
-    SELECT rideable_type FROM [202211-divvy-tripdata]
+    SELECT rideable_type FROM [202211-tripdata]
     UNION ALL
-    SELECT rideable_type FROM [202212-divvy-tripdata]
+    SELECT rideable_type FROM [202212-tripdata]
     UNION ALL
-    SELECT rideable_type FROM [202301-divvy-tripdata]
+    SELECT rideable_type FROM [202301-tripdata]
     UNION ALL
-    SELECT rideable_type FROM [202302-divvy-tripdata]
+    SELECT rideable_type FROM [202302-tripdata]
     UNION ALL
-    SELECT rideable_type FROM [202303-divvy-tripdata]
+    SELECT rideable_type FROM [202303-tripdata]
     UNION ALL
-    SELECT rideable_type FROM [202304-divvy-tripdata]
+    SELECT rideable_type FROM [202304-tripdata]
 ) AS AnnualTable
 GROUP BY  rideable_type;
 rideable_type      totalRides
-classic_bike        2642585
-docked_bike         170518
-electric_bike       3045958
+classic_bike        2639052
+docked_bike         167795
+electric_bike       1727070
