bc7e56c9838925fd72d076ad9e73f72256a91fea
Matt Johnson <111515875+MjxSjx@users.noreply.github.com>
2023-06-12 09:55:58 -0500

    Update Dynamic sum of rows across all sheets.sql

--- Case Study 1 - bike-share analysis/SQL Results/Dynamic sum of rows across all sheets.sql
+++ Case Study 1 - bike-share analysis/SQL Results/Dynamic sum of rows across all sheets.sql
@@ -1,33 +1,33 @@
 -- This syntax felt too pendantic, but it gets the job done. Below is a dynamic alternative to getting the same result.
   
 SELECT SUM(TotalRows) AS TotalRows FROM (
-    SELECT COUNT(*) AS TotalRows FROM [202205-divvy-tripdata]
+    SELECT COUNT(*) AS TotalRows FROM [202205-tripdata]
 		UNION ALL
-    SELECT COUNT(*) AS TotalRows FROM [202206-divvy-tripdata]
+    SELECT COUNT(*) AS TotalRows FROM [202206-tripdata]
 		UNION ALL
-	SELECT COUNT(*) AS TotalRows FROM [202207-divvy-tripdata]
+	SELECT COUNT(*) AS TotalRows FROM [202207-tripdata]
 		UNION ALL
-	SELECT COUNT(*) AS TotalRows FROM [202208-divvy-tripdata]
+	SELECT COUNT(*) AS TotalRows FROM [202208-tripdata]
 		UNION ALL
-	SELECT COUNT(*) AS TotalRows FROM [202209-divvy-tripdata]
+	SELECT COUNT(*) AS TotalRows FROM [202209-tripdata]
 		UNION ALL
-	SELECT COUNT(*) AS TotalRows FROM [202210-divvy-tripdata]
+	SELECT COUNT(*) AS TotalRows FROM [202210-tripdata]
 		UNION ALL
-	SELECT COUNT(*) AS TotalRows FROM [202211-divvy-tripdata]
+	SELECT COUNT(*) AS TotalRows FROM [202211-tripdata]
 		UNION ALL
-	SELECT COUNT(*) AS TotalRows FROM [202212-divvy-tripdata]
+	SELECT COUNT(*) AS TotalRows FROM [202212-tripdata]
 		UNION ALL
-	SELECT COUNT(*) AS TotalRows FROM [202301-divvy-tripdata]
+	SELECT COUNT(*) AS TotalRows FROM [202301-tripdata]
 		UNION ALL
-	SELECT COUNT(*) AS TotalRows FROM [202302-divvy-tripdata]
+	SELECT COUNT(*) AS TotalRows FROM [202302-tripdata]
 		UNION ALL
-	SELECT COUNT(*) AS TotalRows FROM [202303-divvy-tripdata]
+	SELECT COUNT(*) AS TotalRows FROM [202303-tripdata]
 		UNION ALL
-    SELECT COUNT(*) AS TotalRows FROM [202304-divvy-tripdata]
+    SELECT COUNT(*) AS TotalRows FROM [202304-tripdata]
 ) AS AnnualData;
 
 TotalRows
-5,859,061
+4,533,917
 
 ---------------------------------------------------------------------------------------------------------------------------------
 
@@ -39,10 +39,10 @@ SELECT @sql = @sql + N'
     UNION ALL'
 FROM sys.tables
 WHERE name IN (
-    '202205-divvy-tripdata', '202206-divvy-tripdata', '202207-divvy-tripdata',
-    '202208-divvy-tripdata', '202209-divvy-tripdata', '202210-divvy-tripdata',
-    '202211-divvy-tripdata', '202212-divvy-tripdata', '202301-divvy-tripdata',
-    '202302-divvy-tripdata', '202303-divvy-tripdata', '202304-divvy-tripdata'
+    '202205-tripdata', '202206-tripdata', '202207-tripdata',
+    '202208-tripdata', '202209-tripdata', '202210-tripdata',
+    '202211-tripdata', '202212-tripdata', '202301-tripdata',
+    '202302-tripdata', '202303-tripdata', '202304-tripdata'
 );
 
 SET @sql = LEFT(@sql, LEN(@sql) - LEN('UNION ALL'));
@@ -52,4 +52,4 @@ SET @sql = @sql + N') AS AnnualData;'
 EXEC sp_executesql @sql;
 
 TotalRows
-5,859,061
+4,533,917
