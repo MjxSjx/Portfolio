865a5d115fd8cd713ec1bf0307a04813c6309100
Matt Johnson <111515875+MjxSjx@users.noreply.github.com>
2023-06-12 06:06:19 -0500

    Update Dynamic max value of 'ride_length' across all sheets.sql

--- Case Study 1 - bike-share analysis/SQL Results/Individual Statistical Details/Dynamic max value of 'ride_length' across all sheets.sql
+++ Case Study 1 - bike-share analysis/SQL Results/Individual Statistical Details/Dynamic max value of 'ride_length' across all sheets.sql
@@ -1,29 +1,29 @@
  -- The results surprise no one, as more investigation is needed. Pedantic free dynamic version as well.
  
 SELECT MAX(ride_length) AS MaxRL FROM (
-    SELECT ride_length FROM [202205-divvy-tripdata]
+    SELECT ride_length FROM [202205-tripdata]
 		UNION
-    SELECT ride_length FROM [202206-divvy-tripdata]
+    SELECT ride_length FROM [202206-tripdata]
 		UNION
-	SELECT ride_length FROM [202207-divvy-tripdata]
+	SELECT ride_length FROM [202207-tripdata]
 		UNION
-	SELECT ride_length FROM [202208-divvy-tripdata]
+	SELECT ride_length FROM [202208-tripdata]
 		UNION
-	SELECT ride_length FROM [202209-divvy-tripdata]
+	SELECT ride_length FROM [202209-tripdata]
 		UNION
-	SELECT ride_length FROM [202210-divvy-tripdata]
+	SELECT ride_length FROM [202210-tripdata]
 		UNION
-	SELECT ride_length FROM [202211-divvy-tripdata]
+	SELECT ride_length FROM [202211-tripdata]
 		UNION
-	SELECT ride_length FROM [202212-divvy-tripdata]	
+	SELECT ride_length FROM [202212-tripdata]	
 		UNION
-	SELECT ride_length FROM [202301-divvy-tripdata]
+	SELECT ride_length FROM [202301-tripdata]
 		UNION
-	SELECT ride_length FROM [202302-divvy-tripdata]
+	SELECT ride_length FROM [202302-tripdata]
 		UNION
-	SELECT ride_length FROM [202303-divvy-tripdata]
+	SELECT ride_length FROM [202303-tripdata]
 		UNION
-    SELECT ride_length FROM [202304-divvy-tripdata]
+    SELECT ride_length FROM [202304-tripdata]
 ) AS AnnualData;
 MaxRL
 9:59:32
@@ -38,10 +38,10 @@ SELECT @sql = @sql + N'
     UNION'
 FROM sys.tables
 WHERE name IN (
-    '202205-divvy-tripdata', '202206-divvy-tripdata', '202207-divvy-tripdata',
-    '202208-divvy-tripdata', '202209-divvy-tripdata', '202210-divvy-tripdata',
-    '202211-divvy-tripdata', '202212-divvy-tripdata', '202301-divvy-tripdata',
-    '202302-divvy-tripdata', '202303-divvy-tripdata', '202304-divvy-tripdata'
+    '202205-tripdata', '202206-tripdata', '202207-tripdata',
+    '202208-tripdata', '202209-tripdata', '202210-tripdata',
+    '202211-tripdata', '202212-tripdata', '202301-tripdata',
+    '202302-tripdata', '202303-tripdata', '202304-tripdata'
 );
 
 SET @sql = LEFT(@sql, LEN(@sql) - LEN('UNION')); 
