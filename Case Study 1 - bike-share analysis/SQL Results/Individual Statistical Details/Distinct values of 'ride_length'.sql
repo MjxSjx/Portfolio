382bc71182b3111319277fac1a9e46c2ff585f4d
Matt Johnson <111515875+MjxSjx@users.noreply.github.com>
2023-06-12 09:36:19 -0500

    Update Distinct values of 'ride_length'.sql

--- Case Study 1 - bike-share analysis/SQL Results/Individual Statistical Details/Distinct values of 'ride_length'.sql
+++ Case Study 1 - bike-share analysis/SQL Results/Individual Statistical Details/Distinct values of 'ride_length'.sql
@@ -3,36 +3,47 @@
 SELECT COUNT(DISTINCT ride_length) AS DistinctRL FROM [202205-tripdata];
 DistinctRL
 10929
+
 SELECT COUNT(DISTINCT ride_length) AS DistinctRL FROM [202206-tripdata];
 DistinctRL
 11378
+
 SELECT COUNT(DISTINCT ride_length) AS DistinctRL FROM [202207-tripdata];
 DistinctRL
 11432
+
 SELECT COUNT(DISTINCT ride_length) AS DistinctRL FROM [202208-tripdata];
 DistinctRL
 10644
+
 SELECT COUNT(DISTINCT ride_length) AS DistinctRL FROM [202209-tripdata];
 DistinctRL
 9805
+
 SELECT COUNT(DISTINCT ride_length) AS DistinctRL FROM [202210-tripdata];
 DistinctRL
 8514
+
 SELECT COUNT(DISTINCT ride_length) AS DistinctRL FROM [202211-tripdata];
 DistinctRL
 6083
+
 SELECT COUNT(DISTINCT ride_length) AS DistinctRL FROM [202212-tripdata];
 DistinctRL
 4297
+
 SELECT COUNT(DISTINCT ride_length) AS DistinctRL FROM [202301-tripdata];
 DistinctRL
 4216
+
 SELECT COUNT(DISTINCT ride_length) AS DistinctRL FROM [202302-tripdata];
 DistinctRL
 4825
+
 SELECT COUNT(DISTINCT ride_length) AS DistinctRL FROM [202303-tripdata];
 DistinctRL
 5131
+
 SELECT COUNT(DISTINCT ride_length) AS DistinctRL FROM [202304-tripdata];
 DistinctRL
 8006
