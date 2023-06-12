eddc3f16e0e4a508e29dc2c23364601fe53596a6
Matt Johnson <111515875+MjxSjx@users.noreply.github.com>
2023-06-12 10:07:07 -0500

    Update Group by member_casual total.sql

--- Case Study 1 - bike-share analysis/SQL Results/Group by member_casual total.sql
+++ Case Study 1 - bike-share analysis/SQL Results/Group by member_casual total.sql
@@ -2,31 +2,31 @@
 
 SELECT member_casual, COUNT(*) AS totalRides
 FROM (
-    SELECT member_casual FROM [202205-divvy-tripdata]
+    SELECT member_casual FROM [202205-tripdata]
     UNION ALL
-    SELECT member_casual FROM [202206-divvy-tripdata]
+    SELECT member_casual FROM [202206-tripdata]
     UNION ALL
-    SELECT member_casual FROM [202207-divvy-tripdata]
+    SELECT member_casual FROM [202207-tripdata]
     UNION ALL
-    SELECT member_casual FROM [202208-divvy-tripdata]
+    SELECT member_casual FROM [202208-tripdata]
     UNION ALL
-    SELECT member_casual FROM [202209-divvy-tripdata]
+    SELECT member_casual FROM [202209-tripdata]
     UNION ALL
-    SELECT member_casual FROM [202210-divvy-tripdata]
+    SELECT member_casual FROM [202210-tripdata]
     UNION ALL
-    SELECT member_casual FROM [202211-divvy-tripdata]
+    SELECT member_casual FROM [202211-tripdata]
     UNION ALL
-    SELECT member_casual FROM [202212-divvy-tripdata]
+    SELECT member_casual FROM [202212-tripdata]
     UNION ALL
-    SELECT member_casual FROM [202301-divvy-tripdata]
+    SELECT member_casual FROM [202301-tripdata]
     UNION ALL
-    SELECT member_casual FROM [202302-divvy-tripdata]
+    SELECT member_casual FROM [202302-tripdata]
     UNION ALL
-    SELECT member_casual FROM [202303-divvy-tripdata]
+    SELECT member_casual FROM [202303-tripdata]
     UNION ALL
-    SELECT member_casual FROM [202304-divvy-tripdata]
+    SELECT member_casual FROM [202304-tripdata]
 ) AS AnnualTable
 GROUP BY  member_casual;
 member_casual   totalRides
-casual           2358307
-member           3500754
+casual           1791065
+member           2742852
