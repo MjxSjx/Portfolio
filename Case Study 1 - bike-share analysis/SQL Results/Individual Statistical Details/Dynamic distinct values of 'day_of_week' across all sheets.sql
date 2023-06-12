00ed13e7a039ad68362c6044eecc85151f2bb128
Matt Johnson <111515875+MjxSjx@users.noreply.github.com>
2023-06-12 03:55:28 -0500

    Update Dynamic distinct values of 'day_of_week' across all sheets.sql

--- Case Study 1 - bike-share analysis/SQL Results/Individual Statistical Details/Dynamic distinct values of 'day_of_week' across all sheets.sql
+++ Case Study 1 - bike-share analysis/SQL Results/Individual Statistical Details/Dynamic distinct values of 'day_of_week' across all sheets.sql
@@ -1,29 +1,29 @@
 -- Again, i'm not here to judge what information a stakeholder seeks. Only to fullfill the role using data. Dynamic version included. 
  
 SELECT COUNT(DISTINCT day_of_week) AS DistinctDoW FROM (
-    SELECT day_of_week FROM [202205-divvy-tripdata]
+    SELECT day_of_week FROM [202205-tripdata]
     UNION
-    SELECT day_of_week FROM [202206-divvy-tripdata]
+    SELECT day_of_week FROM [202206-tripdata]
     UNION
-    SELECT day_of_week FROM [202207-divvy-tripdata]
+    SELECT day_of_week FROM [202207-tripdata]
     UNION
-    SELECT day_of_week FROM [202208-divvy-tripdata]
+    SELECT day_of_week FROM [202208-tripdata]
     UNION
-    SELECT day_of_week FROM [202209-divvy-tripdata]
+    SELECT day_of_week FROM [202209-tripdata]
     UNION
-    SELECT day_of_week FROM [202210-divvy-tripdata]
+    SELECT day_of_week FROM [202210-tripdata]
     UNION
-    SELECT day_of_week FROM [202211-divvy-tripdata]
+    SELECT day_of_week FROM [202211-tripdata]
     UNION
-    SELECT day_of_week FROM [202212-divvy-tripdata]
+    SELECT day_of_week FROM [202212-tripdata]
     UNION
-    SELECT day_of_week FROM [202301-divvy-tripdata]
+    SELECT day_of_week FROM [202301-tripdata]
     UNION
-    SELECT day_of_week FROM [202302-divvy-tripdata]
+    SELECT day_of_week FROM [202302-tripdata]
     UNION
-    SELECT day_of_week FROM [202303-divvy-tripdata]
+    SELECT day_of_week FROM [202303-tripdata]
     UNION
-    SELECT day_of_week FROM [202304-divvy-tripdata]
+    SELECT day_of_week FROM [202304-tripdata]
 ) AS AnnualData;
 DistinctDoW
 7
@@ -41,10 +41,10 @@ SELECT @sql = @sql + N'
     UNION ALL'
 FROM sys.tables
 WHERE name IN (
-    '202205-divvy-tripdata', '202206-divvy-tripdata', '202207-divvy-tripdata',
-    '202208-divvy-tripdata', '202209-divvy-tripdata', '202210-divvy-tripdata',
-    '202211-divvy-tripdata', '202212-divvy-tripdata', '202301-divvy-tripdata',
-    '202302-divvy-tripdata', '202303-divvy-tripdata', '202304-divvy-tripdata'
+    '202205-tripdata', '202206-tripdata', '202207-tripdata',
+    '202208-tripdata', '202209-tripdata', '202210-tripdata',
+    '202211-tripdata', '202212-tripdata', '202301-tripdata',
+    '202302-tripdata', '202303-tripdata', '202304-tripdata'
 );
 
 SET @sql = LEFT(@sql, LEN(@sql) - LEN('UNION ALL')); -- Remove the last UNION ALL
