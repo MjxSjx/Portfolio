3a56e3df278d6b984675eab45e997f9b3f7ae0ec
Matt Johnson <111515875+MjxSjx@users.noreply.github.com>
2023-06-12 09:35:50 -0500

    Update Distinct values of 'day_of_week'.sql

--- Case Study 1 - bike-share analysis/SQL Results/Individual Statistical Details/Distinct values of 'day_of_week'.sql
+++ Case Study 1 - bike-share analysis/SQL Results/Individual Statistical Details/Distinct values of 'day_of_week'.sql
@@ -3,36 +3,47 @@
 SELECT COUNT(DISTINCT day_of_week) AS DistinctDoW FROM [202205-tripdata]; 
 DistinctDoW
 7
+
 SELECT COUNT(DISTINCT day_of_week) AS DistinctDoW FROM [202206-tripdata];
 DistinctDoW
 7
+
 SELECT COUNT(DISTINCT day_of_week) AS DistinctDoW FROM [202207-tripdata];
 DistinctDoW
 7
+
 SELECT COUNT(DISTINCT day_of_week) AS DistinctDoW FROM [202208-tripdata];
 DistinctDoW
 7
+
 SELECT COUNT(DISTINCT day_of_week) AS DistinctDoW FROM [202209-tripdata];
 DistinctDoW
 7
+
 SELECT COUNT(DISTINCT day_of_week) AS DistinctDoW FROM [202210-tripdata];
 DistinctDoW
 7
+
 SELECT COUNT(DISTINCT day_of_week) AS DistinctDoW FROM [202211-tripdata];
 DistinctDoW
 7
+
 SELECT COUNT(DISTINCT day_of_week) AS DistinctDoW FROM [202212-tripdata];
 DistinctDoW
 7
+
 SELECT COUNT(DISTINCT day_of_week) AS DistinctDoW FROM [202301-tripdata];
 DistinctDoW
 7
+
 SELECT COUNT(DISTINCT day_of_week) AS DistinctDoW FROM [202302-tripdata];
 DistinctDoW
 7
+
 SELECT COUNT(DISTINCT day_of_week) AS DistinctDoW FROM [202303-tripdata];
 DistinctDoW
 7
+
 SELECT COUNT(DISTINCT day_of_week) AS DistinctDoW FROM [202304-tripdata];
 DistinctDoW
 7
