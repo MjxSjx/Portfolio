29d4128aa4214d0ee9c78e40f08d0d52437406a0
Matt Johnson <111515875+MjxSjx@users.noreply.github.com>
2023-06-12 06:49:32 -0500

    Update Mean value of 'ride_length'.sql

--- Case Study 1 - bike-share analysis/SQL Results/Individual Statistical Details/Mean value of 'ride_length'.sql
+++ Case Study 1 - bike-share analysis/SQL Results/Individual Statistical Details/Mean value of 'ride_length'.sql
@@ -1,8 +1,30 @@
 -- Another situation where the variable data type needed to be coerced for SQL to operate, May had the longest ride length at 18:33. 
 
-SELECT AVG(DATEDIFF(second, '00:00:00', ride_length)) AS MeanRL FROM [202205-divvy-tripdata];
-MeanRL
-1113
+SELECT AVG(
+    CAST(SUBSTRING(ride_length, 1, CHARINDEX(':', ride_length) - 1) AS INT) * 3600 +
+    CAST(SUBSTRING(ride_length, CHARINDEX(':', ride_length) + 1, CHARINDEX(':', ride_length, CHARINDEX(':', ride_length) + 1) - CHARINDEX(':', ride_length) - 1) AS INT) * 60 +
+    CAST(SUBSTRING(ride_length, CHARINDEX(':', ride_length, CHARINDEX(':', ride_length) + 1) + 1, LEN(ride_length)) AS INT)
+) AS MeanRLSeconds
+FROM [202205-tripdata]
+WHERE ride_length IS NOT NULL;
+MeanRLSeconds
+1176
+
+
+SELECT AVG(
+    CAST(PARSENAME(REPLACE(ride_length, ':', '.'), 3) AS INT) * 3600 +
+    CAST(PARSENAME(REPLACE(ride_length, ':', '.'), 2) AS INT) * 60 +
+    CAST(PARSENAME(REPLACE(ride_length, ':', '.'), 1) AS INT)
+) AS MeanRLSeconds
+FROM [202205-tripdata]
+WHERE ride_length IS NOT NULL;
+MeanRLSeconds
+1176
+
+What a mess right now because my data type for ride_length is nvarchar......
+
+
+
 SELECT AVG(DATEDIFF(second, '00:00:00', ride_length)) AS MeanRL FROM [202206-divvy-tripdata];
 MeanRL
 1099
