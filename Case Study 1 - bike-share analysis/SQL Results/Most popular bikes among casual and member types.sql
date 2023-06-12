4d14bae3f2db1b18a8191aac7553bc6fb24d4898
Matt Johnson <111515875+MjxSjx@users.noreply.github.com>
2023-06-12 10:11:13 -0500

    Update Most popular bikes among casual and member types.sql

--- Case Study 1 - bike-share analysis/SQL Results/Most popular bikes among casual and member types.sql
+++ Case Study 1 - bike-share analysis/SQL Results/Most popular bikes among casual and member types.sql
@@ -6,36 +6,36 @@ SELECT
   COUNT(*) AS totalRides,
   COUNT(*) * 100.0 / SUM(COUNT(*)) OVER (PARTITION BY member_casual) AS percentage
 FROM (
-  SELECT rideable_type, member_casual FROM [202205-divvy-tripdata]
+  SELECT rideable_type, member_casual FROM [202205-tripdata]
   UNION ALL
-  SELECT rideable_type, member_casual FROM [202206-divvy-tripdata]
+  SELECT rideable_type, member_casual FROM [202206-tripdata]
     UNION ALL
-    SELECT rideable_type, member_casual FROM [202207-divvy-tripdata]
+    SELECT rideable_type, member_casual FROM [202207-tripdata]
     UNION ALL
-    SELECT rideable_type, member_casual FROM [202208-divvy-tripdata]
+    SELECT rideable_type, member_casual FROM [202208-tripdata]
     UNION ALL
-    SELECT rideable_type, member_casual FROM [202209-divvy-tripdata]
+    SELECT rideable_type, member_casual FROM [202209-tripdata]
     UNION ALL
-    SELECT rideable_type, member_casual FROM [202210-divvy-tripdata]
+    SELECT rideable_type, member_casual FROM [202210-tripdata]
     UNION ALL
-    SELECT rideable_type, member_casual FROM [202211-divvy-tripdata]
+    SELECT rideable_type, member_casual FROM [202211-tripdata]
     UNION ALL
-    SELECT rideable_type, member_casual FROM [202212-divvy-tripdata]
+    SELECT rideable_type, member_casual FROM [202212-tripdata]
     UNION ALL
-    SELECT rideable_type, member_casual FROM [202301-divvy-tripdata]
+    SELECT rideable_type, member_casual FROM [202301-tripdata]
     UNION ALL
-    SELECT rideable_type, member_casual FROM [202302-divvy-tripdata]
+    SELECT rideable_type, member_casual FROM [202302-tripdata]
     UNION ALL
-    SELECT rideable_type, member_casual FROM [202303-divvy-tripdata]
+    SELECT rideable_type, member_casual FROM [202303-tripdata]
     UNION ALL
-    SELECT rideable_type, member_casual FROM [202304-divvy-tripdata]
+    SELECT rideable_type, member_casual FROM [202304-tripdata]
 ) AS AnnualTable
 GROUP BY rideable_type, member_casual;
 
 rideable_type 	member_casual	totalRides	percentage
-classic_bike	  casual	  891246	  37.79
-classic_bike	  member	  751339	  50.02
-docked_bike	  casual	  170518   	  7.23
+classic_bike	  casual	  888581	  49.61
+classic_bike	  member	  1750471	  63.82
+docked_bike	  casual	  167795   	  9.36
 docked_bike       member           0               0
-electric_bike 	  casual	  1296543  	  54.98
-electric_bike	  member	  749415  	  49.97
+electric_bike 	  casual	  734689  	  41.01
+electric_bike	  member	  992381  	  36.2
