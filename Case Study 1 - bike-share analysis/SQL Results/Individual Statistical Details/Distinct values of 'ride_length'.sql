2de1598035482e7a16ccb959d22525e2576bfee9
Matt Johnson <111515875+MjxSjx@users.noreply.github.com>
2023-06-12 03:52:32 -0500

    Update Distinct values of 'ride_length'.sql

--- Case Study 1 - bike-share analysis/SQL Results/Individual Statistical Details/Distinct values of 'ride_length'.sql
+++ Case Study 1 - bike-share analysis/SQL Results/Individual Statistical Details/Distinct values of 'ride_length'.sql
@@ -1,38 +1,38 @@
 -- July contains the largest set of unique ride length durations.
  
-SELECT COUNT(DISTINCT ride_length) AS DistinctRL FROM [202205-divvy-tripdata];
+SELECT COUNT(DISTINCT ride_length) AS DistinctRL FROM [202205-tripdata];
 DistinctRL
 10945
-SELECT COUNT(DISTINCT ride_length) AS DistinctRL FROM [202206-divvy-tripdata];
+SELECT COUNT(DISTINCT ride_length) AS DistinctRL FROM [202206-tripdata];
 DistinctRL
 11388
-SELECT COUNT(DISTINCT ride_length) AS DistinctRL FROM [202207-divvy-tripdata];
+SELECT COUNT(DISTINCT ride_length) AS DistinctRL FROM [202207-tripdata];
 DistinctRL
 11453
-SELECT COUNT(DISTINCT ride_length) AS DistinctRL FROM [202208-divvy-tripdata];
+SELECT COUNT(DISTINCT ride_length) AS DistinctRL FROM [202208-tripdata];
 DistinctRL
 10651
-SELECT COUNT(DISTINCT ride_length) AS DistinctRL FROM [202209-divvy-tripdata];
+SELECT COUNT(DISTINCT ride_length) AS DistinctRL FROM [202209-tripdata];
 DistinctRL
 9805
-SELECT COUNT(DISTINCT ride_length) AS DistinctRL FROM [202210-divvy-tripdata];
+SELECT COUNT(DISTINCT ride_length) AS DistinctRL FROM [202210-tripdata];
 DistinctRL
 8520
-SELECT COUNT(DISTINCT ride_length) AS DistinctRL FROM [202211-divvy-tripdata];
+SELECT COUNT(DISTINCT ride_length) AS DistinctRL FROM [202211-tripdata];
 DistinctRL
 6083
-SELECT COUNT(DISTINCT ride_length) AS DistinctRL FROM [202212-divvy-tripdata];
+SELECT COUNT(DISTINCT ride_length) AS DistinctRL FROM [202212-tripdata];
 DistinctRL
 4297
-SELECT COUNT(DISTINCT ride_length) AS DistinctRL FROM [202301-divvy-tripdata];
+SELECT COUNT(DISTINCT ride_length) AS DistinctRL FROM [202301-tripdata];
 DistinctRL
 4216
-SELECT COUNT(DISTINCT ride_length) AS DistinctRL FROM [202302-divvy-tripdata];
+SELECT COUNT(DISTINCT ride_length) AS DistinctRL FROM [202302-tripdata];
 DistinctRL
 4825
-SELECT COUNT(DISTINCT ride_length) AS DistinctRL FROM [202303-divvy-tripdata];
+SELECT COUNT(DISTINCT ride_length) AS DistinctRL FROM [202303-tripdata];
 DistinctRL
 5131
-SELECT COUNT(DISTINCT ride_length) AS DistinctRL FROM [202304-divvy-tripdata];
+SELECT COUNT(DISTINCT ride_length) AS DistinctRL FROM [202304-tripdata];
 DistinctRL
 8009
