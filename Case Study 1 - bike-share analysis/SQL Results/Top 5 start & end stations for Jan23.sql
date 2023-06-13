48f831419e3c687994e24ed7eb1d7e70323e5732
Matt Johnson <111515875+MjxSjx@users.noreply.github.com>
2023-06-13 06:45:02 -0500

    Update Top 5 start & end stations for Jan23.sql

--- Case Study 1 - bike-share analysis/SQL Results/Top 5 start & end stations for Jan23.sql
+++ Case Study 1 - bike-share analysis/SQL Results/Top 5 start & end stations for Jan23.sql
@@ -6,11 +6,11 @@ GROUP BY start_station_name
 ORDER BY member_start_count DESC
 
 start_station_name	            member_start_count
-Ellis Ave & 60th St	                    1632
+Ellis Ave & 60th St	                   1632
 University Ave & 57th St	           1617
-Clinton St & Washington Blvd	            1368
+Clinton St & Washington Blvd	           1368
 Kingsbury St & Kinzie St	           1195
-Ellis Ave & 55th St                     	 1131
+Ellis Ave & 55th St                   1131
 
 
 -- Top 5 end stations for members in Jan 23'
@@ -23,9 +23,9 @@ ORDER BY member_end_count DESC
 end_station_name	              member_end_count
 Ellis Ave & 60th St	                     1673
 University Ave & 57th St	             1590
-Clinton St & Washington Blvd	            1487
+Clinton St & Washington Blvd	             1487
 Kingsbury St & Kinzie St	             1255
-Ellis Ave & 55th St                   	1166
+Ellis Ave & 55th St                       	1166
 
 
 
@@ -40,8 +40,8 @@ start_station_name	           casual_start_count
 Shedd Aquarium	                           306
 Ellis Ave & 60th St	                   285
 Streeter Dr & Grand Ave	                   282
-University Ave & 57th St	             262
-Sheffield Ave & Fullerton Ave	             242
+University Ave & 57th St	            262
+Sheffield Ave & Fullerton Ave	            242
 
 
 -- Top 5 end stations for casual users in Jan 23'
@@ -52,8 +52,8 @@ GROUP BY end_station_name
 ORDER BY casual_end_count DESC
 
 end_station_name	             casual_end_count
-Streeter Dr & Grand Ave	                  321
-University Ave & 57th St	            314
-Millennium Park	                          287
-Ellis Ave & 60th St	                  259
-Shedd Aquarium	                         236
+Streeter Dr & Grand Ave	                   321
+University Ave & 57th St	          314
+Millennium Park	                           287
+Ellis Ave & 60th St	                   259
+Shedd Aquarium	                          236
