5a52a64bb412ce2e584c0552a280e4cc4c3f1ee2
Matt Johnson <111515875+MjxSjx@users.noreply.github.com>
2023-06-13 06:46:26 -0500

    Update Top 5 start & end stations for Jan23.sql

--- Case Study 1 - bike-share analysis/SQL Results/Top 5 start & end stations for Jan23.sql
+++ Case Study 1 - bike-share analysis/SQL Results/Top 5 start & end stations for Jan23.sql
@@ -20,7 +20,7 @@ WHERE member_casual = 'member'
 GROUP BY end_station_name
 ORDER BY member_end_count DESC
 
-end_station_name	              member_end_count
+end_station_name	             member_end_count
 Ellis Ave & 60th St	                   1673
 University Ave & 57th St	           1590
 Clinton St & Washington Blvd	           1487
