7ce05e95b97dd825871cde19629d21ddbe109d9b
Matt Johnson <111515875+MjxSjx@users.noreply.github.com>
2023-06-14 15:12:33 -0500

    Update bike_riders.R

--- Case Study 1 - bike-share analysis/R Results/bike_riders.R
+++ Case Study 1 - bike-share analysis/R Results/bike_riders.R
@@ -17,6 +17,7 @@
 # # # # # # # # # # # # # # # # # # # # # # #  
 
 library(tidyverse)  #helps wrangle data
+# library(hms) #saves you a headache
 library(lubridate)  #helps wrangle date attributes
 library(ggplot2)  #helps visualize data
 getwd() #displays your working directory
@@ -80,9 +81,38 @@ str(db12)
 # Stack individual quarter's data frames into one big data frame
 all_trips <- bind_rows(db1,db2,db3,db4,db5,db6,db7,db8,db9,db10,db11,db12)
 
-# Remove lat, long, birthyear, and gender fields as this data was dropped beginning in 2020
+# Remove lat and long
 all_trips <- all_trips %>%  
   select(-c(start_lat, start_lng, end_lat, end_lng))
+
+
+# Making ride_length the col_type the instructions want "$ ride_length : num" TAKE1??
+#all_trips <- all_trips %>% 
+#  mutate(ride_length = as.numeric(ride_length))
+# this works to create a num format but doesn't 
+# create a col difftime format like the guide assumes you're starting with.
+
+# Making ride_length the col_type the instructions want TAKE 2??
+#all_trips$ride_length <- as.numeric(as.character(all_trips$ride_length))
+# this just breaks things cells become NA
+
+# Making ride_length the col_type the instructions want TAKE 3??
+# Convert "time" column to numeric representation (difftime)
+#all_trips$ride_length <- as.numeric(format(all_trips$ride_length, "%H")) * 3600 +
+#  as.numeric(format(all_trips$ride_length, "%M")) * 60 +
+#  as.numeric(format(all_trips$ride_length, "%S"))
+# this just breaks things cells become NA
+
+# Making ride_length the col_type the instructions want TAKE 4??
+# Convert "time" column to numeric representation (seconds)
+#all_trips$ride_length <- as.numeric(difftime(all_trips$ended_at, all_trips$started_at, units = "secs"))
+
+# Making ride_length the col_type the instructions want TAKE 5??
+all_trips$ride_length <- as.numeric(as.POSIXlt(all_trips$ride_length, format = "%H:%M:%S"))
+
+
+# Notice the change to ride_length
+str(all_trips)
 
 #======================================================
 # STEP 3: CLEAN UP AND ADD DATA TO PREPARE FOR ANALYSIS
@@ -95,32 +125,23 @@ head(all_trips)  #See the first 6 rows of data frame.  Also tail(all_trips)
 str(all_trips)  #See list of columns and data types (numeric, character, etc)
 summary(all_trips)  #Statistical summary of data. Mainly for numerics
 
+
 # There are a few problems we will need to fix:
-# (1) In the "member_casual" column, there are two names for members ("member" and "Subscriber") and two names for casual riders ("Customer" and "casual"). We will need to consolidate that from four to two labels.
-# (2) The data can only be aggregated at the ride-level, which is too granular. We will want to add some additional columns of data -- such as day, month, year -- that provide additional opportunities to aggregate the data.
-# (3) We will want to add a calculated field for length of ride since the 2020Q1 data did not have the "tripduration" column. We will add "ride_length" to the entire dataframe for consistency.
-# (4) There are some rides where tripduration shows up as negative, including several hundred rides where Divvy took bikes out of circulation for Quality Control reasons. We will want to delete these rides.
+# (1) The data can only be aggregated at the ride-level, which is too granular. We will want to add some additional columns of data -- such as day, month, year -- that provide additional opportunities to aggregate the data.
+# (2) We will want to add a calculated field for length of ride since the 2020Q1 data did not have the "tripduration" column. We will add "ride_length" to the entire dataframe for consistency.
+# (3) There are some rides where tripduration shows up as negative, including several hundred rides where Divvy took bikes out of circulation for Quality Control reasons. We will want to delete these rides.
 
-# In the "member_casual" column, replace "Subscriber" with "member" and "Customer" with "casual"
-# Before 2020, Divvy used different labels for these two types of riders ... we will want to make our dataframe consistent with their current nomenclature
-# N.B.: "Level" is a special property of a column that is retained even if a subset does not contain any values from a specific level
-# Begin by seeing how many observations fall under each usertype
-table(all_trips$member_casual)
 
-# Reassign to the desired values (we will go with the current 2020 labels)
-all_trips <-  all_trips %>% 
-  mutate(member_casual = recode(member_casual
-                                ,"Subscriber" = "member"
-                                ,"Customer" = "casual"))
-
-# Check to make sure the proper number of observations were reassigned
-table(all_trips$member_casual)
 
 # Add columns that list the date, month, day, and year of each ride
 # This will allow us to aggregate ride data for each month, day, or year ... before completing these operations we could only aggregate at the ride level
 # https://www.statmethods.net/input/dates.html more on date formats in R found at that link
-all_trips$date <- as.POSIXct(all_trips$started_at, format = "%Y-%m-%d %H:%M:%S")
-all_trips$date <- as.Date(all_trips$started_at) #The default format is yyyy-mm-dd
+
+#all_trips$date <- as.POSIXct(all_trips$started_at, format = "%Y-%m-%d %H:%M:%S") # DO NOT RUN THIS!!!
+
+all_trips$date <- as.Date(all_trips$started_at, format = "%m/%d/%Y %H:%M") #mine
+
+# all_trips$date <- as.Date(all_trips$started_at) #The default format is yyyy-mm-dd yet our format is mm-dd-yyyy
 all_trips$month <- format(as.Date(all_trips$date), "%m")
 all_trips$day <- format(as.Date(all_trips$date), "%d")
 all_trips$year <- format(as.Date(all_trips$date), "%Y")
@@ -128,15 +149,29 @@ all_trips$day_of_week <- format(as.Date(all_trips$date), "%A")
 
 # Add a "ride_length" calculation to all_trips (in seconds)
 # https://stat.ethz.ch/R-manual/R-devel/library/base/html/difftime.html
-all_trips$ride_length <- difftime(all_trips$ended_at,all_trips$started_at)
+#all_trips$ride_length <- difftime(all_trips$ended_at,all_trips$started_at) #doesn't work
+
+#library(hms)
+#library(lubridate)
+#all_trips$ended_at <- as.POSIXct(all_trips$ended_at, format = "%m/%d/%Y %H:%M")
+#all_trips$started_at <- as.POSIXct(all_trips$started_at, format = "%m/%d/%Y %H:%M")
+#all_trips$ride_length <- format(as.duration(all_trips$ended_at - all_trips$started_at), format = "%H:%M:%S")
+
+
 
 # Inspect the structure of the columns
 str(all_trips)
 
+
+
 # Convert "ride_length" from Factor to numeric so we can run calculations on the data
-XXXXXXXXXXXXX is.factor(all_trips$ride_length)
-all_trips$ride_length <- as.numeric(as.character(all_trips$ride_length))
+# is.factor(all_trips$ride_length)
+# all_trips$ride_length <- as.numeric(as.character(all_trips$ride_length)) # this doesn't work because we're a time data type 
+# we dont need this
 is.numeric(all_trips$ride_length)
+# we want true 
+
+
 
 # Remove "bad" data
 # The dataframe includes a few hundred entries when bikes were taken out of docks and checked for quality by Divvy or ride_length was negative
@@ -144,14 +179,23 @@ is.numeric(all_trips$ride_length)
 # https://www.datasciencemadesimple.com/delete-or-drop-rows-in-r-with-conditions-2/
 all_trips_v2 <- all_trips[!(all_trips$start_station_name == "HQ QR" | all_trips$ride_length<0),]
 
+
+
 #=====================================
 # STEP 4: CONDUCT DESCRIPTIVE ANALYSIS
 #=====================================
 # Descriptive analysis on ride_length (all figures in seconds)
-mean(all_trips_v2$ride_length) #straight average (total ride length / rides)
+#all_trips <- all_trips %>% 
+#  mutate(ride_length = as.numeric(ride_length))
+
+mean(all_trips_v2$ride_length) #straight average (total ride length / rides) # doesn't currently work.
+# [1] 992.4241
 median(all_trips_v2$ride_length) #midpoint number in the ascending array of ride lengths
+# [1] 618
 max(all_trips_v2$ride_length) #longest ride
+# [1] 89965
 min(all_trips_v2$ride_length) #shortest ride
+# [1] 0
 
 # You can condense the four lines above to one line using summary() on the specific attribute
 summary(all_trips_v2$ride_length)
@@ -162,49 +206,77 @@ aggregate(all_trips_v2$ride_length ~ all_trips_v2$member_casual, FUN = median)
 aggregate(all_trips_v2$ride_length ~ all_trips_v2$member_casual, FUN = max)
 aggregate(all_trips_v2$ride_length ~ all_trips_v2$member_casual, FUN = min)
 
-# See the average ride time by each day for members vs casual users
-aggregate(all_trips_v2$ride_length ~ all_trips_v2$member_casual + all_trips_v2$day_of_week, FUN = mean)
-
 # Notice that the days of the week are out of order. Let's fix that.
 all_trips_v2$day_of_week <- ordered(all_trips_v2$day_of_week, levels=c("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"))
 
-# Now, let's run the average ride time by each day for members vs casual users
-aggregate(all_trips_v2$ride_length ~ all_trips_v2$member_casual + all_trips_v2$day_of_week, FUN = mean)
+
+# See the average ride time by each day for members vs casual users
+#aggregate(all_trips_v2$ride_length ~ all_trips_v2$member_casual + all_trips_v2$day_of_week, FUN = mean)
+# mine rounds up to leave easier to understand numbers. 
+aggregate(all_trips_v2$ride_length ~ all_trips_v2$member_casual + all_trips_v2$day_of_week, FUN = function(x) round(mean(x), 2))
+
+all_trips_v2 <- all_trips_v2 %>%
+  mutate(started_at = as.POSIXct(started_at, format = "%m/%d/%Y %H:%M"))
+
 
 # analyze ridership data by type and weekday
 all_trips_v2 %>% 
-  mutate(weekday = wday(started_at, label = TRUE)) %>%  #creates weekday field using wday()
-  group_by(member_casual, weekday) %>%  #groups by usertype and weekday
-  summarise(number_of_rides = n()							#calculates the number of rides and average duration 
-            ,average_duration = mean(ride_length)) %>% 		# calculates the average duration
-  arrange(member_casual, weekday)								# sorts
+  mutate(weekday = wday(started_at, label = TRUE)) %>%  
+  group_by(member_casual, weekday) %>%  
+  summarise(number_of_rides = n(),  
+            average_duration = mean(ride_length)) %>%  
+  arrange(member_casual, weekday)
 
+
+					
 # Let's visualize the number of rides by rider type
-all_trips_v2 %>% 
-  mutate(weekday = wday(started_at, label = TRUE)) %>% 
-  group_by(member_casual, weekday) %>% 
-  summarise(number_of_rides = n()
-            ,average_duration = mean(ride_length)) %>% 
-  arrange(member_casual, weekday)  %>% 
+all_trips_v2 %>%
+  mutate(weekday = wday(started_at, label = TRUE)) %>%
+  group_by(member_casual, weekday) %>%
+  summarise(number_of_rides = n(),
+            average_duration = mean(ride_length)) %>%
+  arrange(member_casual, weekday) %>%
   ggplot(aes(x = weekday, y = number_of_rides, fill = member_casual)) +
-  geom_col(position = "dodge")
+  geom_col(position = "dodge") +
+  scale_y_continuous(labels = scales::comma) +
+  labs(title = "Total Annual Rides by Weekday")
+
+
 
 # Let's create a visualization for average duration
-all_trips_v2 %>% 
-  mutate(weekday = wday(started_at, label = TRUE)) %>% 
-  group_by(member_casual, weekday) %>% 
-  summarise(number_of_rides = n()
-            ,average_duration = mean(ride_length)) %>% 
-  arrange(member_casual, weekday)  %>% 
+all_trips_v2 %>%
+  mutate(weekday = wday(started_at, label = TRUE)) %>%
+  group_by(member_casual, weekday) %>%
+  summarise(number_of_rides = n(),
+            average_duration = mean(ride_length)) %>%
+  arrange(member_casual, weekday) %>%
   ggplot(aes(x = weekday, y = average_duration, fill = member_casual)) +
-  geom_col(position = "dodge")
+  geom_col(position = "dodge") +
+  labs(title = "Average Annual Ride Duration by Weekday")
+
+
+all_trips_v2 %>%
+  mutate(weekday = wday(started_at, label = TRUE)) %>%
+  group_by(member_casual, weekday) %>%
+  summarise(number_of_rides = n(),
+            average_duration = mean(ride_length)) %>%
+  arrange(member_casual, weekday) %>%
+  ggplot(aes(x = weekday, y = average_duration / 60, fill = member_casual)) +
+  geom_col(position = "dodge") +
+  scale_y_continuous(labels = scales::comma) +
+  labs(title = "Average Annual Ride Duration by Weekday", y = "Average Duration (minutes)")
+
+
 
 #=================================================
 # STEP 5: EXPORT SUMMARY FILE FOR FURTHER ANALYSIS
 #=================================================
 # Create a csv file that we will visualize in Excel, Tableau, or my presentation software
 # N.B.: This file location is for a Mac. If you are working on a PC, change the file location accordingly (most likely "C:\Users\YOUR_USERNAME\Desktop\...") to export the data. You can read more here: https://datatofish.com/export-dataframe-to-csv-in-r/
-counts <- aggregate(all_trips_v2$ride_length ~ all_trips_v2$member_casual + all_trips_v2$day_of_week, FUN = mean)
-write.csv(counts, file = '~/Desktop/Divvy_Exercise/avg_ride_length.csv')
+#counts <- aggregate(all_trips_v2$ride_length ~ all_trips_v2$member_casual + all_trips_v2$day_of_week, FUN = mean)
+#write.csv(counts, file = '~/Desktop/Divvy_Exercise/avg_ride_length.csv')
+
+file_path <- "D:/Merit/Merit Projects/Previous 12 months of trip data - CSV/avg_ride_length.csv"
+write.csv(counts, file = file_path, row.names = FALSE)
 
 #You're done! Congratulations!
